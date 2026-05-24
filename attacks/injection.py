d25dff6ade447bafcfbf444d9337adc894c7d1ea
Ludvig Allinger <la224kz@student.lnu.se>
2026-05-24 12:53:42 +0200

    Artifact cleanup

--- attacks/injection.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("logs/merged/attack.csv")
-attack = df.copy()
-
-attack.loc[1200:1260, "pump"] = True
-attack.loc[1200:1260, "outlet"] = False
-
-attack.to_csv("logs/attacks/injection.csv", index=False)
--- attacks/replay.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("logs/merged/attack.csv")
-attack = df.copy()
-
-attack.loc[1400:1460] = df.loc[100:160].values
-
-attack.to_csv("logs/attacks/replay.csv", index=False)
